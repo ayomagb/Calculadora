072b1c2f5e35b2b7d53fc5ac34190719e11e42f5
ayomagb <ayomagb@gmail.com>
2016-10-02 20:53:33 +0200

    Creo paquete y clase ModeloCalculadora.java

---
+++ src/modelo_calculadora/ModeloCalculadora.java
@@ -0,0 +1,33 @@
+package modelo_calculadora;
+
+public class ModeloCalculadora {
+	//atributos
+	private double n1;
+	private double n2;
+	//constructor con parámetros
+	public ModeloCalculadora(double n1, double n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	//consultores
+	public double getN1() {
+		return n1;
+	}
+	public double getN2() {
+		return n2;
+	}
+	//modificadores
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+	//String toString()
+	@Override
+	public String toString() {
+		return "ModeloCalculadora [n1=" + n1 + ", n2=" + n2 + "]";
+	}
+	
+}
